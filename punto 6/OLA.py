3364edf66c32b09970577b920fd180589c7f8886
Matias <mpierdominici@itba.edu.ar>
2019-04-28 12:17:18 -0300

    OLA terminado, falta wsola y psola

--- punto 6/OLA.py
+++ punto 6/OLA.py
@@ -20,12 +20,8 @@ class ola:
         self.sigma
         self.nu
         self.test
-    def tao1(self,i): #funcion de mapeo lienal de M a N
-        #return(i*self.k)
-        #return(i*0)
-        #in_min=self.bw(self.sigma.min())
+    def tao1(self,i):
         in_min=0
-        #self.test=self.ew(self.gama[(len(self.gama)-2)])
         in_max=self.ew(self.gama[(len(self.gama)-2)])
         out_min=0
         return ((i - in_min) * (self.n - out_min) / (in_max - in_min) + out_min)
@@ -45,12 +41,12 @@ class ola:
     def run(self,x,k):
         self.o=0.5
         self.x=x
-        self.n=len(self.x)
-        self.k=k
-        self.m=int(self.n * self.k)
-        self.y=np.zeros(self.m)
+        self.n=len(self.x) #longitud del vector de entrada
+        self.k=k #factor de escalamiento
+        self.m=int(self.n * self.k) #longitud del vector de slida
+        self.y=np.zeros(self.m) #vector de salida
         self.wl=int((1/20)*41000) #genero el tamanio de la vantana para qeu entr un périodo de la senial mas grabe
-        self.nu=(1-self.o)*self.wl
+        self.nu=(1-self.o)*self.wl #overlapping
         self.gama=np.zeros(mt.ceil(self.m/self.nu))
         self.sigma=np.zeros(len(self.gama))
         self.gama[0] = 1
@@ -65,36 +61,26 @@ class ola:
         for i in range(0,len(self.sigma)-1):
             self.sigma[i]=self.tao1(self.gama[i])
 
-            #print(self.sigma[i])
-
-
-
         #overlap and add
-
 
         for i in range(2,len(self.sigma)-2):
             frame=self.myProduct(self.x[self.bw(self.sigma[i]): self.ew(self.sigma[i])],self.w)
- #           if( (self.ew(self.gama[i])) >= (len(self.x))):
-#                print("tu vieja")
             if(len(frame)!=self.wl):
                 frame=np.concatenate((frame,np.zeros((self.wl-len(frame)))))
-            #print(len(self.y[self.bw(self.gama[i]):(self.ew(self.gama[i]),self.m)]))
- #           print('min '+str(self.bw(self.gama[i]))+' max '+str(self.ew(self.gama[i]))+' sigma ' +'min '+str(self.bw(self.sigma[i]))+' max '+str(self.ew(self.sigma[i])))
-            #print(len(frame))
-
-            #print(len(self.y[self.bw(self.gama[i]): (self.ew(self.gama[i]))]))
+            #print('min '+str(self.bw(self.gama[i]))+' max '+str(self.ew(self.gama[i]))+' sigma ' +'min '+str(self.bw(self.sigma[i]))+' max '+str(self.ew(self.sigma[i])))
             self.y[self.bw(self.gama[i]): (self.ew(self.gama[i]))] =  self.y[self.bw(self.gama[i]): (self.ew(self.gama[i]))] + frame
 
-#Audio, Fs = sf.read('guitarra.wav')
+
 
 Fs=41000 #frecuencia de sampleo
-f=200
-timeVector= np.arange(0,1,1/Fs)
-Audio= (np.sin(2*pi*f*timeVector) + np.sin(2*50*pi*f*timeVector) + np.sin(2*100*pi*f*timeVector))/3
-#Audio= np.sin(2*pi*f*timeVector)
+f=20
+timeVector= np.arange(0,10,1/Fs)
+#Audio, Fs = sf.read('guitarra.wav')
+#Audio= (np.sin(2*pi*f*timeVector) + np.sin(2*50*pi*f*timeVector) + np.sin(2*100*pi*f*timeVector))/3
+Audio= np.sin(2*pi*f*timeVector)
 
 abc=ola()
-abc.run(Audio,2)
+abc.run(Audio,0.05)
 #sd.play(abc.y,Fs)
 #sd.wait()
 #sf.write('out.wav',abc.y,Fs)
@@ -109,6 +95,8 @@ frecVector = fftfreq(n)
 espectroVector = fft(Audio)
 mask = frecVector>0
 truefft=2.0*np.abs(espectroVector/n)
+
+plt.figure(3)
 plt.plot(frecVector[mask],truefft[mask],linewidth=4)
 n1=len(abc.y)
 frecVector1 = fftfreq(n1)
@@ -116,23 +104,29 @@ espectroVector1 = fft(abc.y)
 mask1 = frecVector1>0
 truefft1=2.0*np.abs(espectroVector1/n)
 plt.plot(frecVector1[mask1],truefft1[mask1],linewidth=3)
+plt.xlabel('Fs'+'['+str(Fs)+'Hz'+']')
+plt.ylabel('Potencia')
 
 
 
-plt.show()
 
 
 
-plt.show()
 
-#plt.figure(1)
-#plt.plot(timeVector,Audio)
+plt.figure(1)
 
-#n2= len(abc.y)
-#timeVector = np.arange(0, n2*(1/Fs), 1/Fs)
+plt.plot(timeVector,Audio)
+
+n2= len(abc.y)
+timeVector = np.arange(0, n2*(1/Fs), 1/Fs)
 #plt.figure(2)
-#plt.plot(timeVector,abc.y)
-#plt.show()
+
+plt.plot(timeVector,abc.y)
+plt.xlabel('Tiempo[s]')
+plt.ylabel('Amplitud')
+
+
+plt.show()
 
 
 
